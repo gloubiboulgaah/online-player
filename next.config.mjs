b67378f06c6e8201c22bc794d1905a63ca3473d1
Elliott <70292987+gloubiboulgaah@users.noreply.github.com>
2025-10-21 09:00:36 +0200

    Refactor next.config.mjs for configuration export

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,4 @@
 /** @type {import('next').NextConfig} */
-// const nextConfig = {};
+const nextConfig = {};
 
-// export default nextConfig;
-
-const nextConfig = {
-    output: "export",
-    images: { unoptimized: true },
-};
 export default nextConfig;
